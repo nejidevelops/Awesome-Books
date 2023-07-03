594ea5d71ece8c8a77ef4764ae6de86bc5be0001
George <georgie.george7@gmail.com>
2023-07-03 18:59:18 +0400

    Eslint fix

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-document.addEventListener('DOMContentLoaded', displayBooks);
-          
-function displayBooks() {
-  const books = getBooks();
-
-  books.forEach((book) => addBookToList(book));
-}
-
+// Display books in the UI
 function addBookToList(book) {
   const list = document.querySelector('#collection');
 
@@ -20,3 +13,72 @@ function addBookToList(book) {
   list.appendChild(bookDisplay);
 }
 
+// Get books from localStorage
+function getBooks() {
+  let books;
+  if (localStorage.getItem('books') === null) {
+    books = [];
+  } else {
+    books = JSON.parse(localStorage.getItem('books'));
+  }
+
+  return books;
+}
+
+const books = getBooks();
+
+function displayBooks() {
+  books.forEach((book) => addBookToList(book));
+}
+
+document.addEventListener('DOMContentLoaded', displayBooks);
+
+// Add a book to localStorage
+function addBook(book) {
+  const books = getBooks();
+  books.push(book);
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+// Remove a book from localStorage
+function removeBook(title) {
+  const books = getBooks();
+
+  books.forEach((book, index) => {
+    if (book.title === title) {
+      books.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('books', JSON.stringify(books));
+}
+// Clear input fields
+function clearFields() {
+  document.querySelector('#title').value = '';
+  document.querySelector('#author').value = '';
+}
+// Add book form submission event
+document.querySelector('#book-form').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+
+  const book = {
+    title: title,
+    author: author,
+  };
+
+  addBookToList(book);
+  addBook(book);
+  clearFields();
+});
+
+// Remove book event
+document.querySelector('#collection').addEventListener('click', (event) => {
+  if (event.target.classList.contains('delete')) {
+    event.target.parentElement.remove();
+    const bookTitle = event.target.previousElementSibling.previousElementSibling.textContent;
+    removeBook(bookTitle);
+  }
+});
