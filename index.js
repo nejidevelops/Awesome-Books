92e0bbfa52fe5b1ed6013a4224b86fdd4bde4c94
Newton Ombese <newtonombese1@gmail.com>
2023-07-06 12:08:08 +0300

    Update index.js file

--- index.js
+++ index.js
@@ -99,3 +99,26 @@ class BookCollection {
 
 const collection = new BookCollection();
 collection.initialize();
+
+const welcomeMessage = document.getElementById('welcome-message');
+const booksCollection = document.getElementById('booksCollection');
+const newBook = document.getElementById('addBooks');
+const contact = document.getElementById('contacts');
+
+booksCollection.addEventListener('click', () => {
+  welcomeMessage.style.display = 'none';
+  newBook.style.display = 'block';
+  contact.style.display = 'block';
+});
+
+newBook.addEventListener('click', () => {
+  welcomeMessage.style.display = 'none';
+  booksCollection.style.display = 'block';
+  contact.style.display = 'block';
+});
+
+contact.addEventListener('click', () => {
+  welcomeMessage.style.display = 'none';
+  booksCollection.style.display = 'block';
+  newBook.style.display = 'block';
+});
